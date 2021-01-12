d7d4a5abafa9f3ac89f4b7fc97d7908160772dc0
Jesus Marte <jesusmarte2430@gmail.com>
2021-01-12 13:15:55 -0500

    All templates has been added

---
+++ src/pages/characters.js
@@ -0,0 +1,21 @@
+const Characters = () => {
+  const view = `
+        <div class="characters__inner">
+        <article class="characters__card">
+        <img src="image" alt="name">
+        <h2></h2>
+        </article>
+        <article class="characters__card">
+        <h3>Episodes:</h3>
+        <h3>Status:</h3>
+        <h3>Species:</h3>
+        <h3>Gender</h3>
+        <h3>Origin:</h3>
+        <h3>Last Location:</h3>
+        </article>
+        </div>
+    `;
+  return view;
+};
+
+export default Characters;
